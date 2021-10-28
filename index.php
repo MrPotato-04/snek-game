2aa82cb3c5ffc6fdf2a659c054204d7bc8ffce0c
kuudarashi <creepas21@gmail.com>
2021-10-28 10:28:16 +0200

    test main page made

--- index.php
+++ index.php
@@ -1,16 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="home.css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+<header>
+    <a href="docs/index.php">game</a>
+    <a href="accountSystem/login/index.php">login</a>
+    <a href="accountSystem/register/registerPage.php">register
+
+</header>
 <body>
-    <script>
-        window.onload = function() {
-            window.location.href="accountSystem/login/index.php"
-        }
-    </script>
+<!--     <script>-->
+<!--        window.onload = function() {-->
+<!--            window.location.href="accountSystem/login/index.php"-->
+<!--        }-->
+<!--    </script>-->
 </body>
 </html>
