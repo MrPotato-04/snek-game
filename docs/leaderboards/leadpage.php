43e00e7a04576314331e6d5bd9e3eb63eda7b2c7
MrPotato-04 <80412415+MrPotato-04@users.noreply.github.com>
2021-11-03 12:17:26 +0100

    edit

--- docs/leaderboards/leadpage.php
+++ docs/leaderboards/leadpage.php
@@ -17,14 +17,25 @@
                 <th>Highscore</th>
             </tr>
             <?php
-                while($row = $res->fetch_assoc()) {
+                // while($row = $res->fetch_assoc()) {
 
-                    echo"<tr>><td>".$row['username']."</td></tr>";
-                    echo"<tr>><td>".$row['scores']."</td></tr>";
+                //     echo"<tr>><td>".$row['username']."</td></tr>";
+                //     echo"<tr>><td>".$row['scores']."</td></tr>";
 
-                }
+                // }
             ?>
+            
         </table>
     </div>
+    <?php
+    echo "________________________________";
+    echo "<table>"; // start a table tag in the HTML
+
+    while($row = mysqli_fetch_array($res)){   //Creates a loop to loop through results
+    echo "<tr><td>" . $row['username'] . "</td><td>" . $row['scores'] . "</td></tr>";  //$row['index'] the index here is a field name
+    }
+    
+    echo "</table>"
+    ?>
 </body>
 </html>
--- docs/leaderboards/leadscript.php
+++ docs/leaderboards/leadscript.php
@@ -1,6 +1,6 @@
 <?php
 $dbc = require './../../database/db.php';
-$res = $dbc->query("SELECT * FROM user, scores");
-$row = $res->fetch_assoc();
+$res = $dbc->query("SELECT * FROM `scores`, `user` WHERE iduser = scores.user_iduser GROUP BY user_iduser ORDER BY scores DESC LIMIT 10");
+
 
 ?>
