bddb6e0aa98df1e200561dfa8a800f2a811967f5
Kevin <kvindso@gmail.com>
2021-11-04 17:05:38 +0100

    updated account info

--- accountSystem/accinfo/accinfo.php
+++ accountSystem/accinfo/accinfo.php
@@ -22,16 +22,23 @@
            echo "you can't acces this page without logging in."; 
         } else {
             ?>
-            <?php echo "Unique ID: ".$row['iduser'];?>
+            <label for="UniqueID">Unique ID <br>
+            <?php echo "<input type='text' value='".$row['iduser']."' disabled>";?>
+            </label>
             <br>
-            <?php echo "Username: ".$row['username'];?>
+            <label for="Email">Email <br>
+            <?php echo "<input type='text' value='".$row['email']."' disabled>";?>
+            </label>
             <br>
-            <?php echo "Email: ".$row['email'];?>
+            <label for="Email">Username <br>
+            <?php echo "<input type='text' value='".$row['username']."'>"; ?>
             <br>
-            <?php echo "Password: ".$row['password'];
+            <label for="Password">Password <br>
+            <?php echo "<input type='password' value='".$row['password']."' disabled>";
+            
         }
 
     ?>
-
+    </label>
 </body>
 </html>
