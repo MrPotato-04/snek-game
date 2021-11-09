db03bc8ad781cf2c3f77b5cfd4e3b4bf4a050ad4
Alec <aarnaoutoglou@roc-dev.com>
2021-11-09 14:52:28 +0100

    new user page styling
    added css styling on new user page

--- accountSystem/register/newUser.php
+++ accountSystem/register/newUser.php
@@ -4,12 +4,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/newUserStyle.css">
     <title>Document</title>
 </head>
 <body>
-    <h1>Registraton Succesful! You can now log in</h1>
+<div class="container">
+    <h1>Registration Successful! You can now log in.</h1>
     <a href="./../login/index.php">
-    <input type="submit" name="button" value="Main Menu">
+    <input type="submit" name="button" value="Main Menu" class="button">
     </a>
+</div>
 </body>
 </html>
