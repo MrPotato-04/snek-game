840a0c0e69a9a33799b648bafc4e0e4c30966361
Kevin <kvindso@gmail.com>
2021-11-10 09:50:19 +0100

    you can no change passwords

--- accountSystem/userinfo/pass_success.php
+++ accountSystem/userinfo/pass_success.php
@@ -4,12 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Password Changed Successfully</title>
 </head>
 <body>
-    <h1>Registraton Succesful! You can now log in</h1>
-    <a href="./../login/index.php">
-    <input type="submit" name="button" value="Go to Login">
+    <h1>Password Change Successfully</h1>
+    <a href="userinfo.php">
+    <input type="submit" name="button" value="Go Back">
     </a>
 </body>
 </html>
