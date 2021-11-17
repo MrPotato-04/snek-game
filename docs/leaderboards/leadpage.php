849859a1b578318d77360bdd690978e28f622afd
Alec <aarnaoutoglou@roc-dev.com>
2021-11-17 11:22:26 +0100

    leaderboard css
    restyled leaderboard

--- docs/leaderboards/leadpage.php
+++ docs/leaderboards/leadpage.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>leaderboard</title>
+    <title>Leaderboard</title>
 </head>
 <body>
 
@@ -19,8 +19,8 @@
     $ranking = 1;
     echo "<table>"; // start a table tag in the HTML
    echo "<tr><th>Ranking</th>
-        <th>user</th>
-        <th>high score</th>
+        <th>User</th>
+        <th>High Score</th>
         </tr>
         <tr>";
     while ($row = mysqli_fetch_array($res)) {
