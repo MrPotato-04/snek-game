db971b74890fec93135903ab9cf9926eab2193d3
kuudarashi <creepas21@gmail.com>
2021-11-18 14:00:37 +0100

    Update footer.php
    footer progress

--- common_style/footer.php
+++ common_style/footer.php
@@ -1,5 +1,7 @@
 <footer>
     <div id="footer" class="footer">
-
+        <p>for questions and help click the link <a href="">here</a></p>
+        <p>if you want too make a gamemode suggestion or something else you would like too be added click then <a
+                    href="">here</a></p>
     </div>
 </footer>
