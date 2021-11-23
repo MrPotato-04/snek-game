852a5035b507164a18e39af99769209faa28548e
kuudarashi <creepas21@gmail.com>
2021-11-23 13:34:21 +0100

    Update footer.php

--- common_style/footer.php
+++ common_style/footer.php
@@ -1,17 +1,7 @@
 <footer>
     <div id="footer" class="footer">
         <img src="" alt="">
-        <p>snek on crack</p>
-        <p>contact info <br>
-        Alec <br>
-        Anton <br>
-        Kevin <br>
-        Karsten <br>
-        O'nyll</p>
-        <p>questions & suggestions <br>
-        gamemode suggestions <br>
-        bug reports <br>
-        other questions</p>
-        <p></p>
+        <p>© 2021 snek on crack| All rights reserved</p>
+
     </div>
 </footer>
